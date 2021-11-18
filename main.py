c0a7290f4b124d122c2ae12c860a5b8846279494
Todd Gerdy <toadimous@gmail.com>
2021-11-17 22:57:45 -0500

    commented out battle stats. Added name input.

--- main.py
+++ main.py
@@ -38,9 +38,9 @@ def attack(o_index: int, p_index: int):
     opponent = player_list[o_index]
     round_damage: int = random_damage(p_index)
     print(f"{player_list[p_index].name} deal {round_damage} damage")
-    opponent.health -= round_damage
-    print(f"{opponent.name}'s health is {opponent.health}")
-    if opponent.health < 1:
+    opponent.current_health -= round_damage
+    print(f"{opponent.name}'s health is {opponent.current_health}")
+    if opponent.current_health < 1:
         print(f"{opponent.name} has been defeated")
         player_list.pop(o_index)
     current_teams = [[j for j in player_list if j.team == t_name] for t_name in {x.team for x in player_list}]
@@ -50,22 +50,28 @@ def attack(o_index: int, p_index: int):
 
 
 if __name__ == '__main__':
+    # Game starts
+    player_name = input("Hello Player! What is your name? \n")
+    # Create a player. Give him a weapon
     player_list: List[Player] = [
-        Player('Todd', "A", health=30),
+        Player(player_name, "A", health=30),
     ]
     player_list[0].set_weapon(4, quality_level=5)
+    player_list[0].view_player_stats()
     player_list[0].view_inventory()
+    # Calculate the number of enemies for the battle
     enemy_count = randint(2, 5)
     for num in range(enemy_count):
         name: str = create_enemy_name()
         enemy_player = Player(name, 'B')
         player_list.append(Player(name, 'B'))
 
+    # Split Player list into a List of teams
     teams: List[List[Player]] = [
         [j for j in player_list if j.team == t_name] for t_name in {x.team for x in player_list}
     ]
 
-    while True:
-        for i, p in enumerate(player_list):
-            opp_index: int = select_opponent_index(player_list, i)
-            attack(opp_index, i)
+    # while True:
+    #     for i, p in enumerate(player_list):
+    #         opp_index: int = select_opponent_index(player_list, i)
+    #         attack(opp_index, i)
