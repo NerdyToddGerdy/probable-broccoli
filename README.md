067e2e0a073c4f59084076e89426d672cff02dce
Todd Gerdy <toadimous@gmail.com>
2021-11-18 22:40:20 -0500

    Got the menu working

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Code I am streaming on Twitch
 
 I got the idea for this from the awesome game Clickpocalypse
 Please check it out and the developer's other apps at https://minmaxia.com/
+
+To build my menus I used # https://pypi.org/project/simple-term-menu/
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 # This is a sample Python script.
-
 import sys
 # Press ⇧⌘F11 to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from random import randint, choice
 from typing import List
+
+from simple_term_menu import TerminalMenu
 
 from Player import Player, create_enemy_name
 
@@ -29,49 +30,117 @@ def select_opponent_index(p_list: List[Player], p_index: int) -> int:
     return choice([idx for idx in range(0, len(p_list)) if p_list[idx].team != player_list[p_index].team])
 
 
-def attack(o_index: int, p_index: int):
+def attack(o_index: int, p_index: int, is_battle: bool):
     """
     Attack the opposing player. If player is defeated, remove player.  If only one team remains, end battle.
+    :param is_battle: Checks if the battle is over
     :param o_index: int: enemy index
     :param p_index: int: player's index
     """
+    # get opponent info
     opponent = player_list[o_index]
+    # get damage amount
     round_damage: int = random_damage(p_index)
+    # deal damage
     print(f"{player_list[p_index].name} deal {round_damage} damage")
     opponent.current_health -= round_damage
     print(f"{opponent.name}'s health is {opponent.current_health}")
+    # Check if opponent survived
     if opponent.current_health < 1:
-        print(f"{opponent.name} has been defeated")
-        player_list.pop(o_index)
-    current_teams = [[j for j in player_list if j.team == t_name] for t_name in {x.team for x in player_list}]
-    if len(current_teams) == 1:
-        print(f'Only Team {player_list[0].team} is left with {len(current_teams[0])} players')
-        sys.exit()
+        if opponent.team == 'A':
+            print(f"{opponent.name} has fainted")
+            opponent.current_health = 0
+        else:
+            print(f"{opponent.name} has been defeated")
+            player_list.pop(o_index)
+
+    # Check if other teams are around
+    # current_teams = [[j for j in player_list if j.team == t_name] for t_name in {x.team for x in player_list}]
+    # if len(current_teams) == 1:
+    #     print(f'Only Team {player_list[0].team} is left with {len(current_teams[0])} players')
+    #     is_battle = False
 
 
-if __name__ == '__main__':
+player_list: List[Player] = []
+
+
+def get_xp(enemy_count: int, enemy_level: int) -> int:
+    return enemy_count * enemy_level
+
+
+def battle():
+    # TODO: At beginning of battle, reset player list
+
+    # Get Player
+    # Calculate the number of enemies for the battle
+    enemy_count = randint(2, 5)
+    enemy_level = 1
+    for num in range(enemy_count):
+        name: str = create_enemy_name()
+        player_list.append(Player(name, 'B', level=enemy_level))
+
+    is_battle: bool = True
+
+    xp = get_xp(enemy_count, enemy_level)
+    while is_battle:
+
+        # For each player in the player list, attack an opponent
+        for i, p in enumerate(player_list):
+            # Check if other teams are around
+            print(f"##### {p.name}'s turn #####")
+            # TODO: figure out how to handle the list of player lists can be less than 2 if team A cannot be deleted.
+            current_teams = [[j for j in player_list if j.team == t_name] for t_name in
+                             {x.team for x in player_list if x.current_health > 0}]
+            if len(current_teams) == 1:
+                if current_teams[0][0].team == 'A':
+                    print(f'You Survived an encounter with {enemy_count} baddies for {xp} experience points')
+                print(f'Only Team {player_list[0].team} is left with {len(current_teams[0])} players')
+                player_list[0].add_xp(xp)
+                is_battle = False
+                break
+            else:
+                opp_index: int = select_opponent_index(player_list, i)
+                if p.current_health > 0:
+                    attack(opp_index, i, is_battle)
+                else:
+                    print(f"{p.name} has fainted and cannot attack")
+
+    # Create Enemies
+    # Start Battle
+    # Add delay between rounds
+    # Check if battle is over.
+    pass
+
+
+def main_menu():
+    options = ["Player stats", "Inventory", "Battle", "Quit"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    print(f"You have selected {options[menu_entry_index]}!")
+    if menu_entry_index == 0:
+        player_list[0].view_player_stats()
+    elif menu_entry_index == 1:
+        player_list[0].view_inventory()
+    elif menu_entry_index == 2:
+        battle()
+    elif menu_entry_index == 3:
+        print("Buh Bye!")
+        sys.exit(1)
+
+
+def start():
     # Game starts
     player_name = input("Hello Player! What is your name? \n")
     # Create a player. Give him a weapon
-    player_list: List[Player] = [
-        Player(player_name, "A", health=30),
-    ]
+    player_list.append(
+        Player(player_name, "A", health=30)
+    )
     player_list[0].set_weapon(4, quality_level=5)
-    player_list[0].view_player_stats()
-    player_list[0].view_inventory()
-    # Calculate the number of enemies for the battle
-    enemy_count = randint(2, 5)
-    for num in range(enemy_count):
-        name: str = create_enemy_name()
-        enemy_player = Player(name, 'B')
-        player_list.append(Player(name, 'B'))
+    while True:
+        main_menu()
 
-    # Split Player list into a List of teams
-    teams: List[List[Player]] = [
-        [j for j in player_list if j.team == t_name] for t_name in {x.team for x in player_list}
-    ]
 
-    # while True:
-    #     for i, p in enumerate(player_list):
-    #         opp_index: int = select_opponent_index(player_list, i)
-    #         attack(opp_index, i)
+
+
+if __name__ == '__main__':
+    start()
